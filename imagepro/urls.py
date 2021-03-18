3a4899fb25b3ffa0c221c614a84258d4c4c2c824
EC2 Default User <ec2-user@ip-172-31-62-91.ec2.internal>
2021-03-18 23:40:52 +0000

    update

--- imagepro/urls.py
+++ imagepro/urls.py
@@ -23,10 +23,12 @@ SOFTWARE.
 '''
 from django.conf.urls import include, url
 from myapp import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns=[
     url(r'^$', views.home),	
     url(r'^process/$', views.process)
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
