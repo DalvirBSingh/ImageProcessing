321682299f9d8a738d648967086357dfcf9f23b3
Dalvir Singh <dalvir.singh@cerner.com>
2021-03-17 23:03:38 -0400

    Update settings.py

--- imagepro/settings.py
+++ imagepro/settings.py
@@ -49,11 +49,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = [
-    'ec2-3-83-160-226.compute-1.amazonaws.com',
-    'ec2-34-201-66-140.compute-1.amazonaws.com',
-    '127.0.0.1'
-]
+ALLOWED_HOSTS = ['*']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
